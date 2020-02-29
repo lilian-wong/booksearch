f0baede0628666c64874da905d1013fddc538599
lilian-wong <lilianwongai@gmail.com>
2020-02-29 17:23:52 -0500

    add booksearch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+
 import BooksearchApp from './booksearchApp/BooksearchApp';
 
-function App() {
-  return (
-    <div className = "App">
-      <BooksearchApp />
-    </div>
-    
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state ={
+      booklist: [],
+      showBooklist: false
+    }
+  }
+  
+  componentDidMount() {
+    const url = "https://www.googleapis.com/books/v1/volumes?q=Henry I";
+    // const url = "https://www.googleapis.com/books/v1/volumes?q=flowers&filter=free-ebooks";
+    // const option = {
+    //   method: 'GET',
+    //   headers:{
+    //     "key": "",
+    //     "content-Type":"application/json"
+    //   },
+    // }
+    // fetch(url,option)
+    fetch(url)
+    .then(response=> {
+      if(!response.ok){
+        throw new Error('Something went wrong, please try again later.');
+      }
+      return response;
+    })
+    .then(response => response.json())
+    .then(result => {
+      this.setState({
+        booklist: result
+      });
+    }
+      )
+    .catch(error =>{
+      console.error('Error: ' + error);
+    })
+  }
+
+
+  render(){
+    return (
+      <div className = "App">
+        <BooksearchApp booklist={this.state.booklist}/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/booksearchApp/BooksearchApp.js
+++ src/booksearchApp/BooksearchApp.js
@@ -2,12 +2,47 @@ import React from 'react';
 import './BooksearchApp.css';
 
 class BooksearchApp extends React.Component{
+    
     render(){
+
+        let booklist = [];
+        if(this.props.booklist.length!==0){
+            console.log(this.props.booklist.items);
+        booklist = this.props.booklist.items.map(
+            (book) => (    
+                <ul className='bookItem'>
+                    <li>{book.volumeInfo.title}</li>
+                    <li>Author: {book.volumeInfo.authors}</li>
+                    <li>{JSON.stringify(this.props.booklist.items[0].searchInfo.textSnippet)}</li>
+                </ul>
+            )  
+        );
+         
+        }
+        
         return(
             <div className="BooksearchApp">
                 <header className="booksearchApp-header">
                     <h1>Google Book Search</h1>
                 </header>
+                <form className="booksearchbar">
+                    <div>
+                    <label htmlFor="search">Search: </label>
+                    <input type="text" className="searchbox" id="searchbox"/>
+                    <button type="submit" className="submitButton">Search</button>
+                    </div>
+                    <div>
+                        <label htmlFor="printType">Print Type: </label>
+                        <select name="printType">
+                            <option default value="All">All</option>
+                        </select>
+                        <select name="bookType">
+                            <option default value="All">No Filter</option>
+                        </select>
+                    </div>
+                </form>
+               {booklist}
+                
             </div>
         )
     }
